b6d2f538a70c764e18385430f46f0a61f4250803
Ana Beatriz Santucci Carmoni <anabeatrizscarmoni@gmail.com>
2026-06-01 20:56:45 -0300

    inital project structure

---
+++ src/main/java/com/github/abeatrizsc/financyx/FinancyxApplication.java
@@ -0,0 +1,13 @@
+package com.github.abeatrizsc.financyx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancyxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancyxApplication.class, args);
+	}
+
+}
